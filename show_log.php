71fd63262a50ad3e071ea0523330e235a49eb87f
Youngil Cho <terzeron@gmail.com>
2016-08-29 18:07:14 +0900

    change the log directory

--- show_log.php
+++ show_log.php
@@ -1,7 +1,7 @@
 <div>
     <?php
     require_once("common.php");
-    $content = file_get_contents("$work_dir/log/all.log");
+    $content = file_get_contents("$work_dir/logs/all.log");
     txt2html($content);
     ?>
 </div>
