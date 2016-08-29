ce385139a3b83bc9b40c71daf64cd74949ff1021
Youngil Cho <terzeron@gmail.com>
2016-08-29 18:11:59 +0900

    change the log directory

--- find_problems.php
+++ find_problems.php
@@ -1,7 +1,7 @@
 <div>
     <?php
     require_once("common.php");
-    $content = file_get_contents("$work_dir/log/find_problems.log");
+    $content = file_get_contents("$work_dir/logs/find_problems.log");
     txt2html($content);
     ?>
 </div>
