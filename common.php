0be28c56a29ea3ca134188b77197320d34bf377b
Youngil Cho <terzeron@gmail.com>
2018-03-13 00:32:40 +0900

    modify json encoding

--- common.php
+++ common.php
@@ -26,18 +26,18 @@ function txt2html($content)
         if (preg_match("/^\s*$/", $line) || $line == "Warning: can't get old list!") {
             continue;
         } elseif (preg_match("/^=====\s+([^=]+)\s+=====\s*$/", $line)) {
-	    if ($div_open == 1) {
-		print "</div>\n";
-		print "</div>\n";
-		$div_open = 0;
-	    }
+	        if ($div_open == 1) {
+		        print "</div>\n";
+		        print "</div>\n";
+		        $div_open = 0;
+	        }
             $line = preg_replace("/^=====\s+([^=]+)\s+=====\s*$/", "<div class='panel panel-default'>\n<div class='panel-heading'>$1</div>\n<div class='panel-body'>\n", $line);
-	    print_line($line);
-	    $div_open = 1;
-	} elseif (preg_match("//", $line)) {
+	        print_line($line);
+	        $div_open = 1;
+	    } elseif (preg_match("//", $line)) {
             $line = preg_replace("/^---\s+([^\-]+)\s+---\s*$/", "<h5>$1</h5>\n", $line);
-	    print_line($line);
-	}
+	        print_line($line);
+	    }
     }
 }
 
@@ -59,27 +59,27 @@ function scan_dirs($work_dir)
     $id2conf_map = array();
     $category_list = array();
     if (is_dir($work_dir)) {
-	if ($dh = opendir($work_dir)) {
-	    while (($dir1 = readdir($dh))) {
-		$category_dir_path = $work_dir . "/" . $dir1;
-		if ($dir1 == "." or $dir1 == ".." or $dir1[0] == "." or $dir1[0] == "_" or !is_dir($category_dir_path) or $dir1 == "test") {
-		    continue;
-		}
-		array_push($category_list, $dir1);
-		if ($dh2 = opendir($category_dir_path)) {
-		    while (($dir2 = readdir($dh2))) {
-			$conf_file_path = $work_dir . "/" . $dir1 . "/" .  $dir2 . "/conf.xml";
-			if ($dir2 == "." or $dir2 == "..") {
-			    continue;
-			}
-			if (is_file($conf_file_path)) {
-			    $conf = read_conf_file($conf_file_path);
-			    $id2conf_map[$dir2] = $conf;
-			}
-		    }
-		}
+	    if ($dh = opendir($work_dir)) {
+	        while (($dir1 = readdir($dh))) {
+		        $category_dir_path = $work_dir . "/" . $dir1;
+		        if ($dir1 == "." or $dir1 == ".." or $dir1[0] == "." or $dir1[0] == "_" or !is_dir($category_dir_path) or $dir1 == "test") {
+		            continue;
+		        }
+		        array_push($category_list, $dir1);
+		        if ($dh2 = opendir($category_dir_path)) {
+		            while (($dir2 = readdir($dh2))) {
+			            $conf_file_path = $work_dir . "/" . $dir1 . "/" .  $dir2 . "/conf.xml";
+			            if ($dir2 == "." or $dir2 == "..") {
+			                continue;
+			            }
+			            if (is_file($conf_file_path)) {
+			                $conf = read_conf_file($conf_file_path);
+			                $id2conf_map[$dir2] = $conf;
+			            }
+		            }
+		        }
+	        }
 	    }
-	}
     }
     return array($id2conf_map, $category_list);
 }
@@ -87,20 +87,20 @@ function scan_dirs($work_dir)
 function determine_current_feed_dir($work_dir, $feed_name)
 {
     if (!$feed_name) {
-	return "";
+	    return "";
     }
     if (is_dir($work_dir)) {
         if ($dh = opendir($work_dir)) {
-	    while (($dir = readdir($dh))) {
+	        while (($dir = readdir($dh))) {
                 if (is_dir($work_dir . "/" . $dir) and $dh2 = opendir($work_dir . "/" . $dir)) {
-		    while (($file = readdir($dh2))) {
+		            while (($file = readdir($dh2))) {
                         if ($file == $feed_name) {
-			    $feed_dir = $dir;
-			    break;
+			                $feed_dir = $dir;
+			                break;
                         }
-		    }
+		            }
                 }
-	    }
+	        }
         }
     }
     return $feed_dir;
@@ -110,7 +110,7 @@ function print_id2name_map($id2conf_map)
 {
     print "var id2name_map = { ";
     foreach ($id2conf_map as $id => $conf) {
-	print "'" . $id . "' : '" . get_name_of_feed($conf) . "', ";
+	    print "'" . $id . "' : " . json_encode(get_name_of_feed($conf)) . ", ";
     }
     print "};";
 }
