ffd15fee2fa478dc303c62fb47df7bda563a6d03
Youngil Cho <terzeron@gmail.com>
2021-03-31 14:17:22 +0900

    modify an utility function 'read_config'

--- common.php
+++ common.php
@@ -9,12 +9,32 @@ $www_dir = $home_dir . "/public_html";
 $message = "";
 $dir = "xmls";
 
-
 list($id2conf_map, $category_list) = scan_dirs($work_dir);
+
+function read_config($config_file)
+{
+    $fp = fopen($config_file, "r");
+    $config_str = fread($fp, 4096);
+    fclose($fp);
+    $config = json_decode($config_str, $assoc = true);
+    return $config;
+}
+
+function is_logged_in()
+{
+    global $_SESSION;
+    return isset($_SESSION) and array_key_exists("fb_access_token", $_SESSION);
+}
+
+function is_admin()
+{
+    global $_SESSION;
+    return is_logged_in() and $_SESSION["user_mail"] == $_SESSION["admin_email"];
+}
 
 function is_client_local_ip() {
     $ip = getenv('REMOTE_ADDR');
-    if (strstr($ip, "192.168.1.")) {
+    if (strstr($ip, "192.168.")) {
         return True;
     }
     return False;
@@ -67,7 +87,7 @@ function txt2html($content)
     }
 }
 
-function read_conf_file($conf_file_path)
+function read_feed_conf_file($conf_file_path)
 {
     $xml_data = simplexml_load_file($conf_file_path);
     return $xml_data;
@@ -99,7 +119,7 @@ function scan_dirs($work_dir)
 			                continue;
 			            }
 			            if (is_file($conf_file_path)) {
-			                $conf = read_conf_file($conf_file_path);
+			                $conf = read_feed_conf_file($conf_file_path);
                             $conf["feed_dir"] = $dir1;
 			                $id2conf_map[$dir2] = $conf;
 			            }
@@ -142,5 +162,13 @@ function print_id2name_map($id2conf_map)
     }
     print "};";
 }
+
+$config = read_config(dirname(__FILE__) . "/oauth/conf.json");
+
+$fb_oauth_config = [
+    'app_id' => $config["app_id"],
+    'app_secret' => $config["app_secret"],
+    'default_graph_version' => 'v2.8',
+];
 
 ?>
